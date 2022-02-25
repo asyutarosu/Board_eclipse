d2b73fb9009cb55d7c9412068bc86434f5c757f9
Fukai <magi.asyutarosu-zagan@ezweb.ne.jp>
2022-02-25 16:55:11 +0900

    掲示板の機能追加

--- src/main/java/com/example/board_eclipse/repository/PostRepository.java
+++ src/main/java/com/example/board_eclipse/repository/PostRepository.java
@@ -2,11 +2,21 @@ package com.example.board_eclipse.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-/**
+
+ /**
  * 投稿のリポジトリー.
  */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 	
+	/**
+	 * IDで検索する
+	 *
+	 * @param id ID
+	 * @return 投稿
+	 */
+	 public Optional<Post> findById(String id);
+
 }
