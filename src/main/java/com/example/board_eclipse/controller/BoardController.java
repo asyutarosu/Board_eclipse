879453c3533f09472cf7ac5b9a324568ac9bbd1d
Fukai <magi.asyutarosu-zagan@ezweb.ne.jp>
2022-02-25 17:59:36 +0900

    掲示板の機能改善

--- src/main/java/com/example/board_eclipse/controller/BoardController.java
+++ src/main/java/com/example/board_eclipse/controller/BoardController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import java.util.Optional;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.data.domain.Sort;
+import com.example.board_eclipse.validation.GroupOrder;
 
 /**
  * 掲示板のフロントコントローラー.
@@ -44,7 +46,7 @@ public class BoardController {
 	 * @return 一覧を設定したモデル
 	 */
 	private Model setList(Model model) {
-		Iterable<Post> list = repository.findAll();
+		Iterable<Post> list = repository.findByDeletedFalseOrderByUpdatedDateDesc();
 		model.addAttribute("list", list);
 		return model;
 	}
@@ -57,7 +59,7 @@ public class BoardController {
 	 * @return テンプレート
 	 */
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public String create(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+	public String create(@ModelAttribute("form") @Validated(GroupOrder.class) Post form, BindingResult result, Model model) {
 		if (!result.hasErrors()) {
 			repository.saveAndFlush(PostFactory.createPost(form));
 			model.addAttribute("form", PostFactory.newPost());
@@ -91,7 +93,7 @@ public class BoardController {
 	 * @return テンプレート
 	 */
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+	public String update(@ModelAttribute("form") @Validated(GroupOrder.class) Post form, BindingResult result, Model model) {
 		if (!result.hasErrors()) {
 			Optional<Post> post = repository.findById(form.getId());
 			repository.saveAndFlush(PostFactory.updatePost(post.get(), form));
---
+++ src/main/java/com/example/board_eclipse/validation/Group1.java
@@ -0,0 +1,5 @@
+package com.example.board_eclipse.validation;
+
+public interface Group1 {
+
+}
